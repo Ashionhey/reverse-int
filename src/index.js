91e06ad2c03d20671d91173cd6dd0950141a626c
AshionHey <ashion169@gmail.com>
2023-01-06 23:34:24 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   if (n < 0) {
+      n = Math.abs(n)
+   }
+   let nStr = String(n)
+   let i = 0;
+   let result = "";
+
+   while (i < nStr.length) {
+      result = `${nStr[i]}${result}`
+      i++
+   }
+   return result = Number(result)
 }
